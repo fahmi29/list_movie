954729680a1298868cb40906111ba777599fa980
Fahmi A <fdemoy29@gmail.com>
2021-01-12 14:38:36 +0700

    your message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Card } from 'antd';
+class App extends Component{
+  state = {
+    movie : []
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount(){
+   this.getMovie(); 
+  }
+
+  getMovie(){
+    fetch(`https://5f50ca542b5a260016e8bfb0.mockapi.io/api/v1/movies`, {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(json => {
+      // console.log(json);
+      this.setState({
+        movie: json
+      });
+      console.log(this.state.movie);
+    })
+    .catch(error => console.log("error ", error));
+  }
+  render(){
+    return(
+      <div>
+        {this.state.movie.map(movies => (
+          <Card 
+            cover={<img
+              alt="example"
+              src={movies.image}
+              width="300"
+            />}
+          >
+            <p>{movies.title}</p>
+          </Card>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default App;
